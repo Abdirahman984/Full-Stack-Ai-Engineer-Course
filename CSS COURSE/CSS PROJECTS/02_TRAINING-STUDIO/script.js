987b9da7f7dff2fa7a6f2e6c772dabb443ebcaa4
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-06-07 16:00:59 +0300

    GYM TRAINING WEBSITE

---
+++ CSS COURSE/CSS PROJECTS/02_TRAINING-STUDIO/script.js
@@ -0,0 +1,18 @@
+const hunbegar = document.querySelector('.hunbegar');
+const mobileMenu = document.querySelector(".mobile-menu");
+const icons = document.querySelectorAll("i");
+
+hunbegar.addEventListener("click", function (event) {
+    const isVisible = mobileMenu.getAttribute('data-visible');
+    if (isVisible == "true") {
+        // qari hiding
+        mobileMenu.setAttribute('data-visible', "false");
+        icons[0].setAttribute('data-visible', "true");
+        icons[1].setAttribute('data-visible', "false");
+    } else if (isVisible == "false") {
+        // soo bandhig showing
+        mobileMenu.setAttribute('data-visible', "true");
+        icons[0].setAttribute('data-visible', "false");
+        icons[1].setAttribute('data-visible', "true");
+    }
+});
