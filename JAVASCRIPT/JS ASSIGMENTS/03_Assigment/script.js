682ecb41f50881ab99008f6cc2cd9d2aae93523a
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-06-14 15:23:51 +0300

    js Assigment 3

---
+++ JAVASCRIPT/JS ASSIGMENTS/03_Assigment/script.js
@@ -0,0 +1,8 @@
+let isSunny = true 
+let isWeekEnd = false
+
+
+console.log("you can not go to beach", isSunny && isWeekEnd)
+console.log("you can go to beach", isSunny || isWeekEnd)
+console.log("is not sunny ", !isSunny)
+console.log("is weekend ", !isWeekEnd)
