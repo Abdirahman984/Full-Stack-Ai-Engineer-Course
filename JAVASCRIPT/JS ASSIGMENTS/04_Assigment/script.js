c58a6973c9958441fed705977691eab3c0253825
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-06-15 19:49:51 +0300

    Assigmenmt 4 js

---
+++ JAVASCRIPT/JS ASSIGMENTS/04_Assigment/script.js
@@ -0,0 +1,16 @@
+function add (num1, num2){
+return num1 + num2
+}
+
+console.log(add(10,20))
+console.log(add(30,20))
+console.log(add(100,220))
+
+let sumTotal = function (a, b){
+    return a + b
+}
+
+console.log(sumTotal(100, 200))
+console.log(sumTotal(40, 70))
+console.log(sumTotal(10, 200))
+console.log(sumTotal(60, 300))
