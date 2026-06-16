6c6dd05e254a49b341605086a5c27c4d6e93a702
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-06-16 14:44:45 +0300

    Assigment 8 js

---
+++ JAVASCRIPT/JS ASSIGMENTS/08_Assigment/script.js
@@ -0,0 +1,13 @@
+let car = {
+    name: "toyota",
+    made: "chaina",
+    model: "v.8",
+    year: 2011 
+}
+
+console.log(car)
+
+console.log(car.name)
+console.log(car.made)
+console.log(car.model)
+console.log(car.year)
