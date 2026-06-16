ac6a16391cc6312874194f7cf7db8ca2ac02d645
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-06-16 16:03:02 +0300

    Assigment 10 js

---
+++ JAVASCRIPT/JS ASSIGMENTS/10_Assigment/script.js
@@ -0,0 +1,9 @@
+let libary = [
+    {title: "Atomic Habits", auther: "James clear", year:2017},
+    {tiitle: "The 48 lowers of power", auther: "Robert Green", year:2014},
+    { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 }
+    
+]
+
+console.log(libary[0].title)
+console.log(libary[1].auther)
