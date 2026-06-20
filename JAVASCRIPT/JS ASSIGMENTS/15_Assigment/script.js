1e9dd645705e5ddbfaf755a0973f71f02b50b1f7
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-06-20 18:33:31 +0300

    Assigment 15 js

---
+++ JAVASCRIPT/JS ASSIGMENTS/15_Assigment/script.js
@@ -0,0 +1,9 @@
+const people = [
+    {name:"abdirahman", age:19, city:"America"},
+    {name:"Aisha", age:15, city:"Kenya"},
+    {name:"Osman", age:28, ciity:"uk"}
+]
+
+for(let key in people){
+    console.log(people[key])
+}
