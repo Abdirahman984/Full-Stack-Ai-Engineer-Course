e54d14996396549f348514b577f3bc8a1be42c36
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-06-21 10:38:19 +0300

    Assigment 17 js

---
+++ JAVASCRIPT/JS ASSIGMENTS/17-Assigment/script.js
@@ -0,0 +1,10 @@
+let tepearture = 20;
+if(tepearture<0){
+    console.log("very cold")
+} else if(tepearture<15){
+    console.log("cold")
+} else if(tepearture<25){
+    console.log("warm")
+}else{
+    console.log("Hot")
+} 
