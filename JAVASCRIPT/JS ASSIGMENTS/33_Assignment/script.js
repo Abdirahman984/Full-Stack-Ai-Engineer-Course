dd0e08399615e0e4d8d384c30e3411b5dcdb3c5c
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-07-01 11:39:53 +0300

    Assignment 33 js

---
+++ JAVASCRIPT/JS ASSIGMENTS/33_Assignment/script.js
@@ -0,0 +1,10 @@
+let para = document.querySelector(".para")
+let p = document.querySelector("p")
+
+function changeTextContent (){
+    para.textContent = "welcome to my code the fisrt i used DOM its so amzing"
+}
+
+function changeInnerContent (){
+    p.innerHTML = "<div> i change the html tag using div do you see guys"
+}
