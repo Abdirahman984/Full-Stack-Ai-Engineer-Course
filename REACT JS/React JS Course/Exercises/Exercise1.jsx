02e0c8ecc2c1dd1ff0acedd0f0793e7cb5935c2c
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-08-04 15:05:46 +0300

    Exercise 1 Components

---
+++ REACT JS/React JS Course/Exercises/Exercise1.jsx
@@ -0,0 +1,10 @@
+function UserCard (){
+    return (
+        <div>
+            <h2>abdirahman</h2>
+            <p>abdirahmanali@gmail.com</p>
+        </div>
+    )
+}
+
+export default UserCard;
