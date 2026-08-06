ba5a4c6b5ef1c8df819e4646f5bab697c6d36b1e
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-08-06 11:48:19 +0300

    Exercise 3 Props in react js

---
+++ REACT JS/React JS Course/Exercises/Exercise-3/PropsExercise.jsx
@@ -0,0 +1,12 @@
+function UserCard ({UserName, Email, Role}){
+    return (
+        <>
+        <h2>{UserName}</h2>
+        <h2>{Email}</h2>
+        <h3>{Role}</h3>
+        
+        </>
+    )
+}
+
+export default UserCard;
